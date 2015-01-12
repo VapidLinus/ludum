f50c7910e329e88328ee8d36dc73859093d32372
Linus Närkling <VapidLinus@gmail.com>
2015-01-12 15:43:33 +0100

    GitHub being derpy

---
+++ Ludum/TestGame/Wall.cs
@@ -0,0 +1,17 @@
+﻿using Ludum.Engine;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Ludum.TestGame
+{
+	public class Wall : GameObject
+	{
+		public Wall()
+		{
+			var r = AddComponent<ShapeRenderer>();
+			r.SetShape(new RectangleShape(new Vector2f(40, 40)));
+			var b = AddComponent<BoxCollider>();
+			b.Size = new Vector2(40, 40);
+		}
+	}
+}
